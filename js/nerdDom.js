4cf7903a04a12431f82cdbe2930a3141ad34cdd2
Kyle Fisher and Sam Peppard <sam@>
2016-08-15 11:06:13 -0400

    add UI/Business logic

--- js/nerdDom.js
+++ js/nerdDom.js
@@ -1,6 +1,27 @@
 
 $(document).ready(function() {
+  $("form#niches").submit(function(event) {
+    event.preventDefault();
+    var faveNiche = [];
+    var niche1 = $("#niche").val();
+    var niche2 = $("#niche2").val();
+    var niche3 = $("#niche3").val();
+    var niche4 = $("#niche4").val();
+    var niche5 = $("#niche5").val();
+    var niche6 = $("#niche6").val();
 
-  var faveNiche = [];
-  
+    faveNiche.push(niche1, niche2, niche3, niche4, niche5, niche6);
+    console.log(faveNiche);
+
+    var results = "<ol class='list-group-item'>";
+
+    for (var i = 0; i < faveNiche.length; i++) {
+      results += "<li>" + faveNiche[i] + "</li>";
+    }
+    results += "</ol>";
+    
+    $(".nerd-input").html("");
+    $(".nerd-input").append(results);
+
+  });
 }); // end ready
